fbcaec4e10a06e2d0e390f5ac20166e1f33f756a
raduanakhawla <raduanakhawla468@gmail.com>
2021-12-26 10:24:55 +0600

    firebase hosting

--- src/Componrnts/Shared/Firebase/firebase.config.js
+++ src/Componrnts/Shared/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAI1g2xPdQ0PQaTMyJN83iNDW41s_vkhK0",
-  authDomain: "excel-blogging.firebaseapp.com",
-  projectId: "excel-blogging",
-  storageBucket: "excel-blogging.appspot.com",
-  messagingSenderId: "712632879336",
-  appId: "1:712632879336:web:774ab3864fa3153345e15d",
+  apiKey: "AIzaSyAfIAzYhhQEaS6qabz7DA23Ti405w7lZr4",
+  authDomain: "code-blog-f7067.firebaseapp.com",
+  projectId: "code-blog-f7067",
+  storageBucket: "code-blog-f7067.appspot.com",
+  messagingSenderId: "462540829447",
+  appId: "1:462540829447:web:791b674dcaaa5157e68ba2",
 };
 
 export default firebaseConfig;
